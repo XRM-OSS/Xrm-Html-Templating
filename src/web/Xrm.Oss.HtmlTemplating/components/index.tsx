a91fa33caf5b476ab28e69ed8fb221ac0b8f21a8
Florian Krönert <Florian.Kroenert@silflo.de>
2019-01-30 23:25:36 +0100

    Fixed issue in solution config page

--- src/web/Xrm.Oss.HtmlTemplating/components/index.tsx
+++ src/web/Xrm.Oss.HtmlTemplating/components/index.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 
-const config = JSON.parse(unescape(window.location.search.substring(6))) as {htmlField: string; jsonField: string};
+const search = unescape(window.location.search);
+const config = search.indexOf("{") !== -1 ? JSON.parse(search.substring(6)) as {htmlField: string; jsonField: string} : undefined;
 
 const htmlField = config ? config.htmlField : "oss_html";
 const jsonField = config ? config.jsonField : "oss_json";
